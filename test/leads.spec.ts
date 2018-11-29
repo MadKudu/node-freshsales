8286300fff3f90418c1c37175cc513c0f5415f9f
Paul Cothenet <paul@madkudu.com>
2018-11-28 18:21:54 -0800

    First very basic client (#1)
    * Initial commit
    
    * Correct coverage and file references

---
+++ test/leads.spec.ts
@@ -0,0 +1,17 @@
+// import { expect } from 'chai';
+// const FreshSales = require('../lib/client');
+// const freshsales = new FreshSales({ apiKey: 'demo' });
+
+// describe('contacts', function () {
+//   this.timeout(10000);
+
+//   describe('get', function () {
+//     it('should return a batch of contacts', function () {
+//       return hubspot.contacts.get().then(data => {
+//         expect(data).to.be.a('object');
+//         expect(data.contacts).to.be.a('array');
+//         expect(data.contacts[0]).to.be.an('object');
+//       });
+//     });
+//   });
+// });
